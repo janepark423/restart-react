ff90cb9bae249ffa1fa93e4af80d7912c6fea97f
jain <janepark423@gmail.com>
2022-12-20 19:01:16 +0900

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Counter from './Counter';
+import Info from './Info';
 
 // function App() {
 //   return (
-//     <div className="App">     
+//     <div className="App">
 //     </div>
 //   );
 // }
 
-const App = () =>{
-  return <Counter/>;
-}
-
+const App = () => {
+  //return <Counter/>;
+  return <Info />;
+};
 
 export default App;
